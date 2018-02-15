f99ab9d7d2b026d6ba8cea13df64078fdd0eb8c9
Paweł Gardziel <gardzielpawel@gmail.com>
2018-02-15 18:18:42 +0100

    moduł 13.5

--- modules/OSInfo.js
+++ modules/OSInfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 // var Time = require('../modules/time')
 
 
@@ -13,13 +14,13 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 86400 % 60).toFixed(0), 'dni', (uptime / 3600 % 60).toFixed(0), 'godz', (uptime /60 % 60).toFixed(0), 'min',(uptime % 60).toFixed(0), 'sec');
+    console.log('System:'.green, type);
+    console.log('Release:'.blue, release);
+    console.log('CPU model:'.yellow, cpu);
+    console.log('Uptime: '.red, (uptime / 86400 % 30).toFixed(0), 'dni', (uptime / 3600 % 60).toFixed(0), 'godz', (uptime /60 % 60).toFixed(0), 'min',(uptime % 60).toFixed(0), 'sec');
     // console.log(Time.print());
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('User name:'.grey, userInfo.username);
+    console.log('Home dir:'.cyan, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
