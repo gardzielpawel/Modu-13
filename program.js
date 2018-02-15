b9fef1590bb400b5701f2c1a99605c1843fbb72a
Paweł Gardziel <gardzielpawel@gmail.com>
2018-02-15 10:37:31 +0100

    moduł 13.3 wersja 3

--- program.js
+++ program.js
@@ -4,13 +4,18 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if(input !== null)
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!\n');
+        switch(instruction) {
+            case '/version':
+                console.log(process.version);
+                break;
+            case '/exit': 
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            default: 
+                process.stderr.write('Wrong instruction!\n');
         }
         
 });
 
-console.log(process.version);
+
