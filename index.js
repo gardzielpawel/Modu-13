05f8f85fa6594ee7a5a878843d57dfe0b7b02ea7
Paweł Gardziel <gardzielpawel@gmail.com>
2018-02-16 16:28:12 +0100

    moduł 13.7 wer 1

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-var os = require('os');
-var OSinfo = require('./modules/OSInfo');
+var fsReaddir = require('fs-readdir');
 
-
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if(input !== null) {
-        var instruction = input.trim();
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                OSinfo.print();  
-                break; 
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
-    }
-});
-
+fsReaddir('../Moduł 13', function _cb(err, filepaths) {
+    console.log('callback err:', err)
+    console.log('callback res:', filepaths)
+  });
