c75b0a8e3e009e7cef971033b54ef4e6d48df774
Paweł Gardziel <gardzielpawel@gmail.com>
2018-02-16 16:39:58 +0100

    moduł 13.4 wersja 2

--- modules/time.js
+++ modules/time.js
@@ -1,5 +1,5 @@
 function time(uptime) {        
-    console.log((uptime / 86400 % 60).toFixed(0), 'dni',
+    console.log((uptime / 86400 % 30).toFixed(0), 'dni',
         (uptime / 3600 % 60).toFixed(0), 'godz',
         (uptime /60 % 60).toFixed(0), 'min',
         (uptime % 60).toFixed(0), 'sec');
